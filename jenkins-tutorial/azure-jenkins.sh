85d5902f6d5b88377824da9d435a7663535d41b9
elyes97 <elyes.othmani@esprit.tn>
2020-07-09 15:21:06 +0100

    success

--- jenkins-tutorial/azure-jenkins.sh
+++ jenkins-tutorial/azure-jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export resourceGroup=Elyes-Othmani-PFE01
+export resourceGroup=myResourceGroup$RANDOM
 virtualMachine=myVM
 adminUser=azureuser
 pathToKubeConfig=~/.kube/config
@@ -8,7 +8,7 @@ if [ -f $pathToKubeConfig ]
 then
 
     # Create a resource group.
-    #az group create --name $resourceGroup --location westeurope
+    az group create --name $resourceGroup --location westeurope
 
     # Create a new virtual machine, this creates SSH keys if not present.
     az vm create --resource-group $resourceGroup --name $virtualMachine --admin-username $adminUser --image UbuntuLTS --generate-ssh-keys
